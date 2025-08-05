e76a7bc2122b69d26f62e56b547aff2bfddc4cb3
Yonatan Gorovetz <goro5357@gmail.com>
2025-08-05 13:57:23 +0300

    JSX ex1 done

---
+++ my-app/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    let companies = [
+    { name: "Tesla", revenue: 140 },
+    { name: "Microsoft", revenue: 300 },
+    { name: "Google", revenue: 600 }
+  ]
+
+  const showCompany = (name, revenue) => {return <div id={name}>{name} makes {revenue} every year</div>}
+
+  return (
+    <div className="ex-space">
+      <h4 className='ex-title'>Exercise 1</h4>
+      <div className="exercise" id="ex-1">
+        {companies.map(company => showCompany(company.name, company.revenue))}
+      </div>
+    </div>
+  )
+}
+
+export default App
