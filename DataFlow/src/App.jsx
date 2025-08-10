89e2832c6e0aecccf6f47fc98d7b94e024bdbb9d
Yonatan Gorovetz <goro5357@gmail.com>
2025-08-10 16:33:14 +0300

    dataflow ex2

--- DataFlow/src/App.jsx
+++ DataFlow/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Exercise1 from './components/Exercise1'
+import Exercise2 from './components/Exercise2'
 
 function App() {
 
   return (
     <>
       <Exercise1/>
+      <Exercise2/>
     </>
   )
 }
---
+++ DataFlow/src/components/Exercise2.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import Conversation from "./Conversation";
+import List from "./List";
+
+function Exercise2(){
+
+    const [data, setData] = useState({
+
+        displayConversation: null,
+        conversations: [
+            {
+                with: "Laura", convo: [
+                    { text: "Hi", sender: "self" },
+                    { text: "You there?", sender: "self" },
+                    { text: "Yeah, hi, what's up?", sender: "other" }
+                ]
+            },
+            {
+                with: "Dad", convo: [
+                    { text: "Have you finished your school work yet?", sender: "other" },
+                    { text: "Yes.", sender: "self" },
+                    { text: "What do you mean, yes?", sender: "other" },
+                    { text: "??", sender: "self" }
+                ]
+            },
+            {
+                with: "Shoobert", convo: [
+                    { text: "Shoobert!!!", sender: "self" },
+                    { text: "Dude!!!!!!!!", sender: "other" },
+                    { text: "Shooooooooo BERT!", sender: "self" },
+                    { text: "You're my best friend", sender: "other" },
+                    { text: "No, *you're* my best friend", sender: "self" },
+                ]
+            }
+        ]
+    });
+
+    return(
+        <>
+        {data.displayConversation ? <Conversation/> : <List/>}
+        </>
+    )
+}
+
+export default Exercise2
